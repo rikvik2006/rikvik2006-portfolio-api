6dd9015ec9ab766fedc8c087e2e78d263c23a9c6
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-08-13 15:42:38 +0200

    Created a prototype of login

--- src/router/customers/index.ts
+++ src/router/customers/index.ts
@@ -1,23 +1,40 @@
 import { Request, Response, Router } from "express";
+import UsersSchema from "../../database/schemas/Users";
 
 const router = Router()
 
-router.get("", (req: Request, res: Response) => {
+
+router.get("", async (req: Request, res: Response) => {
+    req.session.visited = true;
+
+    let data
+
+    try {
+        data = await UsersSchema.find()
+    } catch (err) {
+        console.log(err);
+    }
+
     res.status(200).send({
-        customers: [
-            {
-                name: "rikvik2006",
-                age: 15,
-            },
-            {
-                name: "GIMMI42PIASTRATO",
-                age: 15,
-            },
-        ]
+        data
     })
 })
 
-router.post("/create", (req, res) => {
+router.post("/create", async (req: Request, res: Response) => {
+    const { email, username, password } = req.body;
+
+    let userDB = await UsersSchema.findOne({ $or: [{ email: email }, { userName: username }] })
+    if (userDB) {
+        res.status(400).send({
+            msg: "User arledy exist",
+        })
+    } else {
+        userDB = await UsersSchema.create({ email: email, userName: username, password: password })
+
+        res.status(201).send({
+            userDB,
+        })
+    }
 
 })
 
