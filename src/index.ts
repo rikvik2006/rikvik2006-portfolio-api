473dc04ed4d5f9215bc7e3317cf48e39b150907a
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-08-28 18:20:02 +0200

    Enable the cors function on express api

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 import router from "./router";
 import { config } from "dotenv";
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: false,
 }));
+app.use(cors());
 
 app.use((req, res, next) => {
     console.log(`${req.method}:${req.url}`)
